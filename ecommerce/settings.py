b537d6b809804d9e6a3e52b7df71d5feb0d6f032
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-07-22 19:07:00 +0200

    adding smodatas

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware' #adding for deploy to heroku!!heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
