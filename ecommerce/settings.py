7e3f609c4ad0fae19832357414dc93030c9ed792
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-07-23 16:17:49 +0200

    asdasdsa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / 'templates')],
+        'DIRS': [os.path.dirname(BASE_DIR ,'store' ,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
