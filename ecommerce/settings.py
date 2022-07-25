e9bf2af4434bc747b511fac5354dddc6a350ef22
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-07-25 13:24:19 +0200

    ASsdsadsa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'd16gb71tfoest3',
-#         'HOST': 'ec2-54-76-43-89.eu-west-1.compute.amazonaws.com',
-#         'PORT': '5432',
-#         'USER': 'lgajxzdanecemo',
-#         'PASSWORD': '002ede5f455468db1fb280b0e6b6d3fc9f7f938a72065a34a56a3bb1d13d039f'
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR , 'db.sqlite3')),                                     #BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd16gb71tfoest3',
+        'HOST': 'ec2-54-76-43-89.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
+        'USER': 'lgajxzdanecemo',
+        'PASSWORD': '002ede5f455468db1fb280b0e6b6d3fc9f7f938a72065a34a56a3bb1d13d039f'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR , 'db.sqlite3')),                                     #BASE_DIR / 'db.sqlite3'
+#     }
+# }
 
 #postgres://lgajxzdanecemo:002ede5f455468db1fb280b0e6b6d3fc9f7f938a72065a34a56a3bb1d13d039f@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/d16gb71tfoest3
 
